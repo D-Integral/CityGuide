545005d00579cb4730a4b773af1942e2b598a0de
Александр Нужный <aleksandrnuznyj@Aleksandrs-MacBook-Pro.local>
2015-07-28 17:57:24 +0300

    Custom cells' layout implemented. D-Integralas: Alex Nuzhniy

--- New Orleans In Pictures/CustomCollectionViewLayout.swift
+++ New Orleans In Pictures/CustomCollectionViewLayout.swift
@@ -21,6 +21,7 @@ class CustomFlowLayout: UICollectionViewFlowLayout {
     var numberOfItemsInRow: Int!
     var heightOfSection = [Int : CGFloat]()
     
+    var currentCellOrigin: CGPoint!
     
     override func prepareLayout() {
         super.prepareLayout()
@@ -57,6 +58,8 @@ class CustomFlowLayout: UICollectionViewFlowLayout {
             
             println("Section \(section) height: \(heightOfSection[section])")
         }
+        
+        currentCellOrigin = CGPointMake(margin, headerSize.height + marginBetweenCells)
     }
     
     override func collectionViewContentSize() -> CGSize {
@@ -74,7 +77,6 @@ class CustomFlowLayout: UICollectionViewFlowLayout {
                     let newLayoutAttributes = layoutAttributesForSupplementaryViewOfKind(UICollectionElementKindSectionHeader, atIndexPath: layoutAttributes.indexPath)
                     allElementsAttributes![index] = newLayoutAttributes
                 }
-                
             case .Cell:
                 let newLayoutAttributes = layoutAttributesForItemAtIndexPath(layoutAttributes.indexPath)
                 allElementsAttributes![index] = newLayoutAttributes
@@ -85,13 +87,100 @@ class CustomFlowLayout: UICollectionViewFlowLayout {
         return allElementsAttributes
     }
     
+    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes! {
+        var attributes = UICollectionViewLayoutAttributes(forCellWithIndexPath: indexPath)
+        
+        attributes.size = indexPath.row == 0 ? itemSizeLarge : itemSizeSmall
+        
+        switch indexPath.section {
+        case 0:
+            if isFirstCellAt(indexPath) {
+                attributes.center = centerForLargeCell()
+                currentCellOrigin.x += itemSizeLarge.width + marginBetweenCells
+            } else {
+                if cellFitsWithinLine() {
+                    if !isRowEvenAt(indexPath) {
+                        attributes.center = centerForSmallCell()
+                        currentCellOrigin.y += itemSizeSmall.height + marginBetweenCells
+                    } else {
+                        attributes.center = centerForSmallCell()
+                        currentCellOrigin.x += itemSizeSmall.width + marginBetweenCells
+                        currentCellOrigin.y -= itemSizeSmall.height + marginBetweenCells
+                    }
+                } else {
+                    currentCellOrigin.x = margin
+                    currentCellOrigin.y += itemSizeLarge.height + marginBetweenCells
+                    
+                    attributes.center = centerForSmallCell()
+                    
+                    currentCellOrigin.y += itemSizeSmall.height + marginBetweenCells
+                }
+            }
+            
+            if isCellLastInSectionAt(indexPath) {
+                currentCellOrigin.x = margin
+                currentCellOrigin.y = heightOfSection[indexPath.section]! + headerSize.height + marginBetweenCells
+            }
+        case 1:
+            if isFirstCellAt(indexPath) {
+                attributes.center = centerForLargeCell()
+                currentCellOrigin.x += itemSizeLarge.width + marginBetweenCells
+            } else {
+                if cellFitsWithinLine() {
+                    if !isRowEvenAt(indexPath) {
+                        attributes.center = centerForSmallCell()
+                        currentCellOrigin.y += itemSizeSmall.height + marginBetweenCells
+                    } else {
+                        attributes.center = centerForSmallCell()
+                        currentCellOrigin.x += itemSizeSmall.width + marginBetweenCells
+                        currentCellOrigin.y -= itemSizeSmall.height + marginBetweenCells
+                    }
+                } else {
+                    currentCellOrigin.x = margin
+                    currentCellOrigin.y += itemSizeLarge.height + marginBetweenCells
+                    
+                    attributes.center = centerForSmallCell()
+                    
+                    currentCellOrigin.y += itemSizeSmall.height + marginBetweenCells
+                }
+            }
+            
+            if isCellLastInSectionAt(indexPath) {
+                currentCellOrigin.x = margin
+                currentCellOrigin.y = heightOfSection[indexPath.section - 1]! + heightOfSection[indexPath.section]! + headerSize.height + marginBetweenCells
+            }
+
+        case 2:
+            if isFirstCellAt(indexPath) {
+                attributes.center = centerForLargeCell()
+                currentCellOrigin.x += itemSizeLarge.width + marginBetweenCells
+            } else {
+                if cellFitsWithinLine() {
+                    if !isRowEvenAt(indexPath) {
+                        attributes.center = centerForSmallCell()
+                        currentCellOrigin.y += itemSizeSmall.height + marginBetweenCells
+                    } else {
+                        attributes.center = centerForSmallCell()
+                        currentCellOrigin.x += itemSizeSmall.width + marginBetweenCells
+                        currentCellOrigin.y -= itemSizeSmall.height + marginBetweenCells
+                    }
+                } else {
+                    currentCellOrigin.x = margin
+                    currentCellOrigin.y += itemSizeLarge.height + marginBetweenCells
+                    
+                    attributes.center = centerForSmallCell()
+                    
+                    currentCellOrigin.y += itemSizeSmall.height + marginBetweenCells
+                }
+            }
+
+        default: break
+        }
     
-//    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes! {
-//        
-//        
-//    }
-//    
-//    
+        //println("Cell attributes at indexpath: row \(indexPath.row), section \(indexPath.section)")
+        return attributes
+    }
+    
     override func layoutAttributesForSupplementaryViewOfKind(elementKind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes! {
         var attributes = UICollectionViewLayoutAttributes(forSupplementaryViewOfKind: elementKind, withIndexPath: indexPath)
         
@@ -111,38 +200,10 @@ class CustomFlowLayout: UICollectionViewFlowLayout {
         return attributes
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
         return true
     }
 
-    
-    
     //MARK: Private helper methods
     
     func contentSize() -> CGSize {
@@ -152,41 +213,35 @@ class CustomFlowLayout: UICollectionViewFlowLayout {
             size.height += heightOfSection[section]!
         }
         
+        size.height += marginBetweenCells
+        
         println("Content size: width \(size.width), height \(size.height)")
         return size
     }
     
+    func isFirstCellAt(indexPath: NSIndexPath) -> Bool {
+        return indexPath.row == 0 ? true : false
+    }
     
+    func isRowEvenAt(indexPath: NSIndexPath) -> Bool {
+        return indexPath.row % 2 == 0 ? true : false
+    }
     
+    func centerForLargeCell() -> CGPoint {
+        return CGPointMake(currentCellOrigin.x + itemSizeLarge.width / 2, currentCellOrigin.y + itemSizeLarge.height / 2)
+    }
     
+    func centerForSmallCell() -> CGPoint {
+        return CGPointMake(currentCellOrigin.x + itemSizeSmall.width / 2, currentCellOrigin.y + itemSizeSmall.height / 2)
+    }
     
+    func cellFitsWithinLine() -> Bool {
+        return self.collectionView!.frame.size.width - currentCellOrigin.x > itemSizeSmall.width + marginBetweenCells ? true : false
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    func isCellLastInSectionAt(indexPath: NSIndexPath) -> Bool {
+        return indexPath.row == numberOfItemsInSection[indexPath.section]! - 1 ? true : false
+    }
     
     //MARK: Animation
     //    override func initialLayoutAttributesForAppearingDecorationElementOfKind(elementKind: String, atIndexPath decorationIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
